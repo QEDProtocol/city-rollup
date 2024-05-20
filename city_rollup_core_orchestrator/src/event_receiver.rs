a3624ca3a15711510af79e2ff570555dc193467a
GopherJ <alex_cj96@foxmail.com>
2024-05-20 13:32:26 +0800

    fix: should use rpc_processor to convert
    Signed-off-by: GopherJ <alex_cj96@foxmail.com>

--- city_rollup_core_orchestrator/src/event_receiver.rs
+++ city_rollup_core_orchestrator/src/event_receiver.rs
@@ -1,5 +1,7 @@
 use std::time::Duration;
 
+use city_redis_store::RedisStore;
+use city_rollup_common::actors::rpc_processor::QRPCProcessor;
 use city_rollup_common::actors::traits::OrchestratorEventReceiverSync;
 use city_rollup_common::api::data::block::requested_actions::*;
 use city_rollup_worker_dispatch::implementations::redis::{
@@ -9,51 +11,99 @@ use city_rollup_worker_dispatch::implementations::redis::{
 use city_rollup_worker_dispatch::traits::proving_worker::ProvingWorkerListener;
 use plonky2::hash::hash_types::RichField;
 
-pub struct CityEventReceiver {
+pub struct CityEventReceiver<F: RichField> {
     dispatcher: RedisDispatcher,
+    rpc_processor: QRPCProcessor<F>,
+    proof_store: RedisStore,
 }
 
-impl<F: RichField> OrchestratorEventReceiverSync<F> for CityEventReceiver {
+impl<F: RichField> CityEventReceiver<F> {
+    pub fn new(
+        dispatcher: RedisDispatcher,
+        rpc_processor: QRPCProcessor<F>,
+        proof_store: RedisStore,
+    ) -> Self {
+        Self {
+            dispatcher,
+            rpc_processor,
+            proof_store,
+        }
+    }
+}
+
+impl<F: RichField> OrchestratorEventReceiverSync<F> for CityEventReceiver<F> {
     fn flush_claim_deposits(&mut self) -> anyhow::Result<Vec<CityClaimDepositRequest>> {
-        let res = self
+        let reqs = self
             .dispatcher
             .pop_all(Q_CLAIM_DEPOSIT)?
             .into_iter()
             .map(|v| Ok(serde_json::from_slice(&v)?))
             .collect::<anyhow::Result<Vec<_>>>()?;
 
+        let mut res = Vec::with_capacity(reqs.len());
+        for req in reqs {
+            res.push(self.rpc_processor.injest_rpc_claim_deposit(
+                &mut self.proof_store,
+                0,
+                &req,
+            )?);
+        }
+
         Ok(res)
     }
 
     fn flush_register_users(&mut self) -> anyhow::Result<Vec<CityRegisterUserRequest<F>>> {
-        let res = self
+        let reqs = self
             .dispatcher
             .pop_all(Q_REGISTER_USER)?
             .into_iter()
             .map(|v| Ok(serde_json::from_slice(&v)?))
             .collect::<anyhow::Result<Vec<_>>>()?;
 
+        let mut res = Vec::with_capacity(reqs.len());
+        for req in reqs {
+            res.push(self.rpc_processor.injest_rpc_register_user(0, &req)?);
+        }
+
         Ok(res)
     }
 
     fn flush_add_withdrawals(&mut self) -> anyhow::Result<Vec<CityAddWithdrawalRequest>> {
-        let res = self
+        let reqs = self
             .dispatcher
             .pop_all(Q_ADD_WITHDRAWAL)?
             .into_iter()
             .map(|v| Ok(serde_json::from_slice(&v)?))
             .collect::<anyhow::Result<Vec<_>>>()?;
 
+        let mut res = Vec::with_capacity(reqs.len());
+        for req in reqs {
+            res.push(self.rpc_processor.injest_rpc_add_withdrawal(
+                &mut self.proof_store,
+                0,
+                &req,
+            )?);
+        }
+
         Ok(res)
     }
 
     fn flush_token_transfers(&mut self) -> anyhow::Result<Vec<CityTokenTransferRequest>> {
-        let res = self
+        let reqs = self
             .dispatcher
             .pop_all(Q_TOKEN_TRANSFER)?
             .into_iter()
             .map(|v| Ok(serde_json::from_slice(&v)?))
             .collect::<anyhow::Result<Vec<_>>>()?;
+
+        let mut res = Vec::with_capacity(reqs.len());
+        for req in reqs {
+            res.push(self.rpc_processor.injest_rpc_token_transfer(
+                &mut self.proof_store,
+                0,
+                &req,
+            )?);
+        }
 
         Ok(res)
     }
