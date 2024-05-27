4b9572f6c9102d2825f6e8f7d40f0bd53ec2841b
GopherJ <alex_cj96@foxmail.com>
2024-05-27 11:15:34 +0800

    debug purpose
    Signed-off-by: GopherJ <alex_cj96@foxmail.com>

--- city_rollup_circuit/src/worker/toolbox/root.rs
+++ city_rollup_circuit/src/worker/toolbox/root.rs
@@ -7,7 +7,7 @@ use city_common_circuit::{
     field::cubic::CubicExtendable,
 };
 use city_crypto::{
-    field::{qfield::QRichField, serialized_2d_felt_bls12381::Serialized2DFeltBLS12381},
+    field::qfield::QRichField,
     hash::{
         merkle::treeprover::TPCircuitFingerprintConfig, qhashout::QHashOut,
         traits::hasher::MerkleZeroHasher,
@@ -277,7 +277,12 @@ impl<S: QProofStoreReaderSync> QWorkerGenericProverGroth16<S, PoseidonGoldilocks
             verifier_data.constants_sigmas_cap.height(),
         );
         let wrapper_proof = wrapper.prove_base(&inner_proof, &verifier_data)?;
-        let (proof_string, _) =  gnark_plonky2_wrapper::wrap_plonky2_proof(wrapper.circuit_data, &wrapper_proof, Some(&format!("/tmp/plonky2_proof/{}/", job_id.task_index)), "/1/")?;
+        let (proof_string, _) = gnark_plonky2_wrapper::wrap_plonky2_proof(
+            wrapper.circuit_data,
+            &wrapper_proof,
+            Some(&format!("/tmp/plonky2_proof/{}/", job_id.task_index)),
+            "/tmp/groth16-keystore/0/",
+        )?;
         // let proof_string = serde_json::to_string(&CityGroth16ProofData {
         //     pi_a: Serialized2DFeltBLS12381([0u8; 48]),
         //     pi_b_a0: Serialized2DFeltBLS12381([0u8; 48]),
