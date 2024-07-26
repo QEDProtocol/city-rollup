bb9e127f5bb779992748398014eca4e99d7561e4
GopherJ <alex_cj96@foxmail.com>
2024-07-27 00:59:45 +0800

    update config to fix potential l1_deposit failure
    Signed-off-by: GopherJ <alex_cj96@foxmail.com>

--- city_rollup_dev_cli/examples/config_permutations.rs
+++ city_rollup_dev_cli/examples/config_permutations.rs
@@ -2,11 +2,15 @@ use std::sync::Arc;
 
 use city_common_circuit::circuits::traits::qstandard::QStandardCircuit;
 use city_crypto::hash::qhashout::QHashOut;
-use city_rollup_circuit::sighash_circuits::{sighash::CRSigHashCircuit, sighash_refund::CRSigHashRefundCircuit};
-use city_rollup_common::introspection::rollup::introspection::{BlockSpendCoreConfig, RefundIntrospectionGadgetConfig};
+use city_rollup_circuit::sighash_circuits::{
+    sighash::CRSigHashCircuit, sighash_refund::CRSigHashRefundCircuit,
+};
+use city_rollup_common::introspection::rollup::introspection::{
+    BlockSpendCoreConfig, RefundIntrospectionGadgetConfig,
+};
 use city_store::store::sighash::SigHashMerkleTree;
-use plonky2::{field::goldilocks_field::GoldilocksField, plonk::config::PoseidonGoldilocksConfig};
 use indicatif::{ProgressBar, ProgressStyle};
+use plonky2::{field::goldilocks_field::GoldilocksField, plonk::config::PoseidonGoldilocksConfig};
 
 pub fn parallelize<T: Send, F: Fn(&mut [T], usize) + Send + Sync + Clone>(v: &mut [T], f: F) {
     let n = v.len();
@@ -37,10 +41,20 @@ fn main() {
     let max_deposits = 4;
     let max_widthdrawals = 4;
     let block_spend_config = BlockSpendCoreConfig::standard_p2sh_p2pkh();
-    let permutations = block_spend_config
-        .generate_permutations(max_deposits, max_widthdrawals);
+    let mut permutations = block_spend_config
+        .generate_permutations(max_deposits, max_widthdrawals)
+        .into_iter()
+        .zip(
+            block_spend_config
+                .generate_id_permutations(max_deposits, max_widthdrawals)
+                .into_iter(),
+        )
+        .collect::<Vec<_>>();
+    permutations.sort_by(|a, b| a.1.cmp(&b.1));
+
     let style = ProgressStyle::default_bar()
-        .template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}").unwrap()
+        .template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}")
+        .unwrap()
         .progress_chars("##-");
 
     let pb = ProgressBar::new(permutations.len() as u64);
@@ -56,7 +70,7 @@ fn main() {
     parallelize(&mut fingerprints[1..], |fingerprints, start| {
         for (i, fingerprint) in fingerprints.iter_mut().enumerate() {
             let idx = start + i;
-            let circuit = CRSigHashCircuit::<C, D>::new(permutations[idx].clone());
+            let circuit = CRSigHashCircuit::<C, D>::new(permutations[idx].0.clone());
             *fingerprint = circuit.get_fingerprint();
             pb.inc(1);
         }
